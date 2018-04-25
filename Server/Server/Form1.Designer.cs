3aa2077d55acba5755ad492724a04554c764ebfc
Upd4ting <upd4ting@gmail.com>
2018-04-25 21:01:09 +0200

    Good progress on the server

--- Server/Server/Form1.Designer.cs
+++ Server/Server/Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.portText = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.button = new System.Windows.Forms.Button();
             this.informationText = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -46,14 +48,14 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Port:";
             // 
-            // textBox1
+            // portText
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(88, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "4269";
+            this.portText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portText.Location = new System.Drawing.Point(88, 24);
+            this.portText.Name = "portText";
+            this.portText.Size = new System.Drawing.Size(100, 26);
+            this.portText.TabIndex = 1;
+            this.portText.Text = "4269";
             // 
             // contextMenuStrip1
             // 
@@ -68,6 +70,7 @@
             this.button.TabIndex = 2;
             this.button.Text = "Start";
             this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
             // informationText
             // 
@@ -77,14 +80,35 @@
             this.informationText.TabIndex = 3;
             this.informationText.Text = "";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Websocket IP:";
+            // 
+            // ipText
+            // 
+            this.ipText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipText.Location = new System.Drawing.Point(339, 21);
+            this.ipText.Name = "ipText";
+            this.ipText.Size = new System.Drawing.Size(142, 26);
+            this.ipText.TabIndex = 5;
+            this.ipText.Text = "4269";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(493, 313);
+            this.Controls.Add(this.ipText);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.informationText);
             this.Controls.Add(this.button);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.portText);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "LeapMotion - Server";
@@ -97,10 +121,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox portText;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Button button;
         private System.Windows.Forms.RichTextBox informationText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ipText;
     }
 }
 
--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Text;
@@ -11,27 +9,154 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Server
-{
+namespace Server {
     public partial class Form1 : Form {
-        private TcpListener _listener;
-        private ClientWebSocket _webSocket;
-        private bool _running;
+        //Leap Motion websokect APIs
+        private const    string GET_FOCUS      = "{\"focused\" : \"true\"}";
+        private const    string LOST_FOCUS     = "{\"focused\" : \"false\"}";
+        private const    string BACKGROUND_ON  = "{\"background\" : \"true\"";
+        private const    string BACKGROUND_OFF = "{\"background\" : \"false\"";
+        private const    string GESTURES_ON    = "{\"enableGestures\" : \"true\"";
+        private const    string GESTURES_OFF   = "{\"enableGestures\" : \"false\"";
+        private const    string HMD_ON         = "{\"optimizeHMD\" : \"true\"";
+        private const    string HMD_OFF        = "{\"optimizeHMD\" : \"false\"";
 
-        public Form1()
-        {
+        private bool   _running;
+
+        private TcpListener     _listener;
+        private ClientWebSocket _webSocket;
+        private Dictionary<TcpClient, int> _clients = new Dictionary<TcpClient, int>();
+        private Dictionary<TcpClient, long> _clientDelay = new Dictionary<TcpClient, long>();
+
+        public Form1() {
             InitializeComponent();
             _running = false;
         }
 
-        private void StartWebSocket() {
+        private async void StartWS() {
             _webSocket = new ClientWebSocket();
+            await _webSocket.ConnectAsync(new Uri("ws://" + ipText.Text + ":20307/v6.json"), CancellationToken.None);
+
+            await SendString(_webSocket, BACKGROUND_ON, CancellationToken.None);
+            await SendString(_webSocket, GET_FOCUS, CancellationToken.None);
+        }
+
+        private void StartWSReceivingThread() {
+            Task.Run(async () => {
+                try {
+                    string frame = await ReadString(_webSocket);
+
+                    long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+                    foreach (TcpClient client in _clients.Keys) {
+                        long lastMilli = _clientDelay[client];
+                        int fps = _clients[client];
+
+                        if (milliseconds - lastMilli >= 1000 / fps) {
+                            _clientDelay[client] = lastMilli;
+
+                            BinaryWriter writer = new BinaryWriter(client.GetStream());
+                            writer.Write(frame);
+                        }
+                    }
+                } catch {
+                    // Swallow exception (close exception)
+                }
+            });
+        }
+
+        private async void StopWS() {
+            await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Just stop", CancellationToken.None);
+        }
+
+        private void StartSocket() {
+            _running = true;
+
+            _listener = new TcpListener(new IPEndPoint(IPAddress.Any, int.Parse(portText.Text)));
+            _listener.Start();
+
+            Task.Run(() => {
+                while (_running) {
+                    try {
+                        TcpClient client = _listener.AcceptTcpClient();
+
+                        Task.Run(() => {
+                            while (_running) {
+                                try {
+                                    BinaryReader reader = new BinaryReader(client.GetStream());
+                                    int fps = reader.ReadInt32();
+
+                                    if (fps == -1) {
+                                        _clients.Remove(client);
+                                        _clientDelay.Remove(client);
+                                        client.Close();
+                                        client.Dispose();
+                                    } else {
+                                        long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                                        _clients[client] = fps;
+                                        _clientDelay[client] = milliseconds;
+                                    }
+                                } catch {
+                                    // Swallow close exception
+                                }
+                            }
+                        });
+                    } catch {
+                        // Swallow close exception
+                    }
+                }
+            });
+        }
+
+        private void StopSocket() {
+            _listener.Stop();
+            _listener = null;
+            _running = false;
+        }
+
+        private void LogInformation(string text) {
+            //TODO
         }
 
         private async void Form1_FormClosing(object sender, FormClosingEventArgs e) {
             if (_running)
                 _listener.Stop();
             await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Just close", CancellationToken.None);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            //TODO START STOP AND CHANGE TEXT
+        }
+
+        public static Task SendString(ClientWebSocket ws, string data, CancellationToken cancellation)
+        {
+            var                encoded = Encoding.UTF8.GetBytes(data);
+            ArraySegment<byte> buffer  = new ArraySegment<byte>(encoded, 0, encoded.Length);
+            return ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancellation);
+        }
+
+        public static async Task<string> ReadString(ClientWebSocket ws)
+        {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[8192]);
+
+            WebSocketReceiveResult result = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                do
+                {
+                    result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                } while (!result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                using (StreamReader reader = new StreamReader(ms, Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
         }
     }
 }
